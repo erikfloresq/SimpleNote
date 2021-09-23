9af88f6bd9c6a48b86481d086f04c3cf1c24ad09
Erik Flores <erik.flores@pedidosya.com>
2021-09-23 19:28:58 -0300

    Add basic setup

--- SimpleNote/ViewController.swift
+++ SimpleNote/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textArea: UITextView!
+    var doneBarButton = UIBarButtonItem(title: "Done",
+                                    style: .done,
+                                    target: self,
+                                    action: #selector(doneAction))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textArea.delegate = self
+
     }
 
+    @objc func doneAction() {
+        textArea.resignFirstResponder()
+        navigationItem.rightBarButtonItem = nil
+    }
+}
 
+extension ViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        navigationItem.rightBarButtonItem = doneBarButton
+    }
+
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        return true
+    }
 }
 
