ac5f5a5fb5c1e4b675dbd59a4d3a9d7428e2ff9e
Erik Flores <erik.flores@pedidosya.com>
2021-09-23 18:16:22 -0300

    Initial Commit

---
+++ SimpleNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleNote
+//
+//  Created by Erik Flores on 23/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
